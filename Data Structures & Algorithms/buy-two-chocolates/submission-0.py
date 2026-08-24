82ee385af3b1684a2eb7fbbadc0d77840f711363
ShaheerAli01 <167102116+ShaheerAli01@users.noreply.github.com>
2026-08-23 22:03:15 -0700

    Add: buy-two-chocolates - submission-0

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices.sort()
+        left = money - (prices[0] + prices[1])
+        if left >= 0:
+            return left
+        return money
+
+
+        
